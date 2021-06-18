1649d90bc6798fc84e571f8647f5c37fe2e10d52
Mr-SteveNascimento <stevenson.melo.silva07@aluno.ifce.edu.br>
2021-06-18 10:35:39 -0300

    Adicionando Validação com Yup

--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useFormik } from "formik";
 import "./styles.css";
+import * as Yup from 'yup'
 
-const validate = values => {
+
+// Validação utilizando apenas o formik
+/* const validate = values => {
     const errors = {};
     if (!values.firstName) {
-      errors.firstName = 'Obrigatório!';
+      errors.firstName = 'Campo Obrigatório!';
     } else if (values.firstName.length > 15) {
       errors.firstName = 'Apenas 15 caracteres ou menos!';
     }
   
     if (!values.lastName) {
-      errors.lastName = 'Obrigatório!';
+      errors.lastName = 'Campo Obrigatório!';
     } else if (values.lastName.length > 20) {
       errors.lastName = 'Apenas 20 caracteres ou menos!';
     }
   
     if (!values.email) {
-      errors.email = 'Obrigatório!';
+      errors.email = 'Campo Obrigatório!';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
       errors.email = 'Endereço de Email Inválido!';
     }
   
     return errors;
-};
+}; */
 
 const SignupForm = () => {
     const formik = useFormik({
@@ -33,7 +36,15 @@ const SignupForm = () => {
         firstName: "",
         lastName: "",
     },
-    validate,
+    validationSchema: Yup.object({
+        firstName: Yup.string()
+            .max(15, "Apenas 15 caracteres ou menos!")
+            .required('Campo Obrigatório!'),
+        lastName: Yup.string()
+            .max(20, 'Apenas 20 caracteres ou menos!')
+            .required('Campo Obrigatório!'),
+        email: Yup.string().email('Endereço de Email Inválido').required('Campo Obrigatório!'),
+    }),
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     }
